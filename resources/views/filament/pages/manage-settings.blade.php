0916c48abd8b0a1e233e329d7138d657a77b3766
iRIANDI <yirbjht100@gmail.com>
2026-08-25 12:15:51 +0800

    fix: replace invalid form component with x-filament::form

--- resources/views/filament/pages/manage-settings.blade.php
+++ resources/views/filament/pages/manage-settings.blade.php
@@ -1,5 +1,5 @@
 <x-filament-panels::page>
-    <x-filament-panels::form wire:submit="submit">
+    <x-filament::form wire:submit="submit">
         {{ $this->form }}
 
         <div class="fi-form-actions">
@@ -7,5 +7,5 @@
                 Save Settings
             </x-filament::button>
         </div>
-    </x-filament-panels::form>
+    </x-filament::form>
 </x-filament-panels::page>
